88b5fad43a0437bee172e2e70c3a5271893a2a98
Jack Grigg <thestr4d@gmail.com>
2021-03-13 17:32:11 +1300

    Remove "final" round logic from poseidon::Pow5T3Chip
    This was a bug in the Poseidon reference implementation, fixed in v1.1.

--- src/circuit/gadget/poseidon/pow5t3.rs
+++ src/circuit/gadget/poseidon/pow5t3.rs
@@ -22,7 +22,6 @@ pub struct Pow5T3Config<F: FieldExt> {
     rc_b: [Column<Fixed>; WIDTH],
     s_full: Selector,
     s_partial: Selector,
-    s_final: Selector,
 
     half_full_rounds: usize,
     half_partial_rounds: usize,
@@ -75,7 +74,6 @@ impl<F: FieldExt> Pow5T3Chip<F> {
 
         let s_full = meta.selector();
         let s_partial = meta.selector();
-        let s_final = meta.selector();
 
         let alpha = [5, 0, 0, 0];
         let pow_5 = |v: Expression<F>| {
@@ -165,17 +163,6 @@ impl<F: FieldExt> Pow5T3Chip<F> {
             partial_round_linear(meta, 2)
         });
 
-        let final_full_round = |meta: &mut ConstraintSystem<F>, idx: usize| {
-            let cur = meta.query_advice(state[idx], Rotation::cur());
-            let next = meta.query_advice(state[idx], Rotation::next());
-            let rc = meta.query_fixed(rc_a[idx], Rotation::cur());
-            let s_final = meta.query_selector(s_final, Rotation::cur());
-            s_final * (pow_5(cur + rc) - next)
-        };
-        meta.create_gate("final full round state_0", |meta| final_full_round(meta, 0));
-        meta.create_gate("final full round state_1", |meta| final_full_round(meta, 1));
-        meta.create_gate("final full round state_2", |meta| final_full_round(meta, 2));
-
         Pow5T3Config {
             state,
             state_permutation,
@@ -184,7 +171,6 @@ impl<F: FieldExt> Pow5T3Chip<F> {
             rc_b,
             s_full,
             s_partial,
-            s_final,
             half_full_rounds,
             half_partial_rounds,
             alpha,
@@ -238,21 +224,12 @@ impl<F: FieldExt> PoseidonInstructions for Pow5T3Chip<F> {
 
                 (0..config.half_full_rounds).fold(Ok(state), |res, r| {
                     res.and_then(|state| {
-                        if r < config.half_full_rounds - 1 {
-                            state.full_round(
-                                &mut region,
-                                &config,
-                                config.half_full_rounds + 2 * config.half_partial_rounds + r,
-                                config.half_full_rounds + config.half_partial_rounds + r,
-                            )
-                        } else {
-                            state.final_round(
-                                &mut region,
-                                &config,
-                                config.half_full_rounds + 2 * config.half_partial_rounds + r,
-                                config.half_full_rounds + config.half_partial_rounds + r,
-                            )
-                        }
+                        state.full_round(
+                            &mut region,
+                            &config,
+                            config.half_full_rounds + 2 * config.half_partial_rounds + r,
+                            config.half_full_rounds + config.half_partial_rounds + r,
+                        )
                     })
                 })
             },
@@ -372,31 +349,6 @@ impl<F: FieldExt> Pow5T3State<F> {
                     r_mid.map(|r| m[0][0] * r[0] + m[0][1] * r[1] + m[0][2] * r[2]),
                     r_mid.map(|r| m[1][0] * r[0] + m[1][1] * r[1] + m[1][2] * r[2]),
                     r_mid.map(|r| m[2][0] * r[0] + m[2][1] * r[1] + m[2][2] * r[2]),
-                ],
-            ))
-        })
-    }
-
-    fn final_round(
-        self,
-        region: &mut Region<Pow5T3Chip<F>>,
-        config: &Pow5T3Config<F>,
-        round: usize,
-        offset: usize,
-    ) -> Result<Self, Error> {
-        Self::round(region, config, round, offset, config.s_final, |_| {
-            let mut new_state = self
-                .0
-                .iter()
-                .zip(config.round_constants[round].iter())
-                .map(|(word, rc)| word.value.map(|v| (v + rc).pow(&config.alpha)));
-
-            Ok((
-                round + 1,
-                [
-                    new_state.next().unwrap(),
-                    new_state.next().unwrap(),
-                    new_state.next().unwrap(),
                 ],
             ))
         })
